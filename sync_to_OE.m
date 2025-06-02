c487c19df6c15673c390608225ee69a7eb544951
Brain8 (David Swygart) <davidswygart@gmail.com>
2025-06-02 12:31:52 -0400

    Create sync_to_OE.m

---
+++ sync_to_OE.m
@@ -0,0 +1,93 @@
+oe_events_path = "/home/lapishla/Desktop/small_test/export/openEphysData11_22_PV1__4634416_1/events.mat";
+video_events_path = "/home/lapishla/Desktop/small_test/videos/8_events.csv";
+% tracking_csv = "/home/lapishla/Desktop/small_test/videos/8DLC_Resnet50_pv2capMay28shuffle1_snapshot_160.csv";
+
+% oe_events_path = "/home/lapishla/Desktop/small_test/export/openEphysData12_05_PV30__4633954_28/events.mat";
+% video_events_path = "/home/lapishla/Desktop/small_test/videos/45_events.csv";
+% tracking_csv = "";
+
+e = load(oe_events_path);
+e = struct2table(e.data);
+v = readtable(video_events_path);
+%% plot ephys events
+figure(1); clf; hold on;
+ON = e.state==0;
+scatter(e.timestamp(ON), e.line(ON), 'filled', 'g')
+scatter(e.timestamp(~ON), double(e.line(~ON))-0.1, 'filled', 'r')
+%% plot video events
+figure(2); clf; hold on;
+ON = v.state==1;
+scatter(v.frame(ON), strcmp(v.side(ON),'R'), 'filled', 'g')
+scatter(v.frame(~ON), strcmp(v.side(~ON),'R')-.1, 'filled', 'r')
+% scatter(e.timestamp(~ON), double(e.line(~ON))-0.1, 'filled', 'r')
+%%
+%video
+sum(v.state==0 & strcmp(v.side,'R')) % 182 , 1
+sum(v.state==0 & strcmp(v.side,'L')) % 216 , 216
+% ephys
+sum(e.state==0 & e.line==1) %216 , 216 == 432
+sum(e.state==0 & e.line==2) %216 , 216 == 432
+%%
+e = sortrows(e,"timestamp","ascend");
+v = sortrows(v,"frame","ascend");
+%%
+v.oe_times(:) = nan;
+% Left ON
+v = add_oe_times(v, 1, 'L', e, 0, 2); %I checked and video L does correspond to line 2 for 2.mp4
+% Left OFF
+v = add_oe_times(v, 0, 'L', e, 1, 2);
+% Right ON
+v = add_oe_times(v, 1, 'R', e, 0, 1);
+% Right OFF
+v = add_oe_times(v, 0, 'R', e, 1, 1);
+
+
+%% functions
+function v = add_oe_times(v, state_v, side_v, e, state_e, line_e)
+bool_v = v.state==state_v  &  strcmp(v.side, side_v);
+bool_e = e.state==state_e  &  e.line==line_e;
+
+v_frames = v.frame(bool_v);
+e_times = e.timestamp(bool_e);
+
+pattern_diff = plot_diff_patterns(v_frames, e_times);
+
+len_v = length(v_frames);
+len_e = length(e_times);
+if len_v ~= len_e
+    warning('video has %d events, but ephys has %d events', len_v, len_e)
+    return
+end
+
+max_pattern_error = max(abs(pattern_diff));
+if max_pattern_error > 0.1
+    warning('max pattern difference of %f', max_pattern_error)
+    return
+end
+
+v.oe_times(bool_v) = e_times;
+
+figure(2); clf
+scatter(v_frames, e_times)
+xlabel('video frame')
+ylabel('ephys time (s)')
+end
+
+
+function pattern_diff = plot_diff_patterns(v_frames, e_times)
+    figure(1); clf; hold on;
+    fps = 15;
+    v_times = v_frames / fps;
+    v_diff = diff(v_times);
+    e_diff = diff(e_times);
+    
+    plot(v_diff)
+    plot(e_diff)
+
+    ylabel('time since last event (s)')
+    xlabel('event number')
+    legend('video', 'ephys')
+
+    min_length = min([length(v_diff), length(e_diff)]);
+    pattern_diff = v_diff(1:min_length) - e_diff(1:min_length);
+end
